bc75ad4ecb958cf154e3c5d3725d53a192345070
yuna <yn060312@gmail.com>
2022-04-02 13:38:29 +0900

    최종 제출

--- codingTest/src/Main.java
+++ codingTest/src/Main.java
@@ -1,14 +1,12 @@
 import java.util.Scanner;
 
-public class App {
-
+public class Main {
+    
     public static void main(String[] args) throws Exception {
         System.out.println("입력하세요");
 
         Scanner scanner = new Scanner(System.in);
-        int num;
-
-        num = scanner.nextInt();
+        int num = scanner.nextInt();
 
         execute(num);
     }
