5654f261aaf1cc4bac4af157d5e7432c47f35a2f
yuna <yn060312@gmail.com>
2022-04-02 20:35:18 +0900

    백준 - 평균은 넘겠지

--- codingTest/src/Main.java
+++ codingTest/src/Main.java
@@ -5,23 +5,43 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        Scanner in = new Scanner(System.in);
+        int testcase = 5;
 
-        int[] arr;
+        List<String> list = new ArrayList<>();
+        list.add("5 50 50 70 80 100");
+        list.add("7 100 95 90 80 70 60 50");
+        list.add("3 70 90 80");
+        list.add("3 70 90 81");
+        list.add("9 100 99 98 97 96 95 94 93 91");
 
-        int cnt = 0;
-        while(in.hasNext()) {
+        excuete(testcase, list);
+    }
+
+    public static void excuete(int testcase, List<String> list) {
+        // 각 케이스마다 한 줄씩 평균을 넘는 학생들의 비율을 반올림하여 소수점 셋째 자리까지 출력한다.
+        for (String score : list) {
+            String[] arr = score.split(" ");
+
+            int stdntCnt = Integer.parseInt(arr[0]); // 학생수
             
-            int testcase = in.nextInt();
-            System.out.println(" testcase : " + testcase);
+            int sum =0;
+            for (int i = 1; i < arr.length; i++) {
+                int num = Integer.parseInt(arr[i]);
+                sum += num;
+            }
             
-            if(testcase > 0){
+            double avg = sum / stdntCnt; // 평균
+            
+            int count = 0;
+            // 평균 넘는 사람들 체크 
+            for (int i = 1; i < arr.length; i++) {
+                int num = Integer.parseInt(arr[i]);
+                if(num > avg) {
+                    count++;
+                }
             }
-
-            if(testcase == 10) {
-                break;
-            }
+            // n명 중에 m명은 몇 % 냐 소수점 3번째까지
+            System.out.println(String.format("%.3f", (double) count / (double) stdntCnt * 100.0));;
         }
-        in.close();
     }
 }
