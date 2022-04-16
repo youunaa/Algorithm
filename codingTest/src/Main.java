4b265494ab29b67338364d155f16c9aea55f55e3
yuna <yn060312@gmail.com>
2022-04-17 01:38:49 +0900

    솔루션 코드 깔끔한 코드 추가

--- codingTest/src/Main.java
+++ codingTest/src/Main.java
@@ -85,4 +85,26 @@ public class Main {
 		return answer;
 	}
 
+	public String solution2(String new_id) {
+        String answer = "";
+        String temp = new_id.toLowerCase();
+
+        temp = temp.replaceAll("[^-_.a-z0-9]","");
+        System.out.println(temp);
+        temp = temp.replaceAll("[.]{2,}",".");
+        temp = temp.replaceAll("^[.]|[.]$","");
+        System.out.println(temp.length());
+        if(temp.equals(""))
+            temp+="a";
+        if(temp.length() >=16){
+            temp = temp.substring(0,15);
+            temp=temp.replaceAll("^[.]|[.]$","");
+        }
+        if(temp.length()<=2)
+            while(temp.length()<3)
+                temp+=temp.charAt(temp.length()-1);
+
+        answer=temp;
+        return answer;
+    }
 }
