ef61af9013a46a0aa95ce3a42643407067d2d9f4
yuna <yn060312@gmail.com>
2022-04-17 01:38:06 +0900

    5단계 오류 수정

--- codingTest/src/Main.java
+++ codingTest/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
 	public static void main(String[] args) {
-		String new_id = "...!@BaT#*..y.abcdefghijklm.";
+		String new_id = "abcdefghijklmn.p";
 		// String new_id = "=.=";
 		solution(new_id);
 	}
@@ -56,16 +56,16 @@ public class Main {
 		// 5단계 new_id가 빈 문자열이라면, new_id에 "a"를 대입합니다.
 		System.out.println("5단계 " + new_id);
 
-		if (new_id.length() > 16) {
+		if (new_id.length() >= 16) {
 			new_id = new_id.substring(0, 15);
 		}
+		System.out.println("6단계 " + new_id);
 		if (".".equals(new_id.substring(new_id.length() - 1, new_id.length()))) {
 			new_id = new_id.substring(0, new_id.length() - 1);
 		}
-
+		System.out.println("6단계 " + new_id);
 		// 6단계 new_id의 길이가 16자 이상이면, new_id의 첫 15개의 문자를 제외한 나머지 문자들을 모두 제거합니다.
 		// 만약 제거 후 마침표(.)가 new_id의 끝에 위치한다면 끝에 위치한 마침표(.) 문자를 제거합니다.
-		System.out.println("6단계 " + new_id);
 
 		// 7단계 new_id의 길이가 2자 이하라면, new_id의 마지막 문자를 new_id의 길이가 3이 될 때까지 반복해서 끝에 붙입니다.
 		if (new_id.length() <= 2) {
