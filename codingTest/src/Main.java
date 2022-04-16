fe3a37b858a774312ac12b8eeee059c2d9465b9e
yuna <yn060312@gmail.com>
2022-04-17 02:12:47 +0900

    프로그래머스 - 크레인 인형뽑기 게임

--- codingTest/src/Main.java
+++ codingTest/src/Main.java
@@ -1,72 +1,15 @@
 public class Main {
 
 	public static void main(String[] args) {
-		String new_id = "abcdefghijklmn.p";
-		// String new_id = "=.=";
-		solution(new_id);
+		int[][] board = {{0,0,0,0,0},{0,0,1,0,3},{0,2,5,0,1},{4,2,4,4,2},{3,5,1,3,1}}
+		int[] moves = {1,5,3,5,1,2,1,4};
+
+		solution(board, moves);
 	}
 
-	/**
-	 * 
-	 * @param new_id
-	 * @return bat.y.abcdefghi
-	 */
-	public static String solution(String new_id) {
-		new_id = new_id.toLowerCase(); // 1단계
-
-		new_id = new_id.replaceAll("[^-_.a-z0-9]",""); // 2단계
-		new_id = new_id.replaceAll("\\.{2,}", "."); // 3단계
-		new_id = new_id.replaceAll("^[.]|[.]$",""); // 4단계
-
-		if ("".equals(new_id)) {
-			new_id = "a";
-		}
-		// 5단계 new_id가 빈 문자열이라면, new_id에 "a"를 대입합니다.
-		if (new_id.length() >= 16) {
-			new_id = new_id.substring(0, 15);
-            new_id = new_id.replaceAll("[.]$","");
-		}
-
-		if (new_id.length() <= 2) {
-			StringBuilder sb1 = new StringBuilder();
-			sb1.append(new_id);
-
-			String lastStr = new_id.substring(new_id.length() - 1, new_id.length());
-
-			while(sb1.length() < 3) {
-				sb1.append(lastStr);
-			}
-			new_id = sb1.toString();
-		}
-		
-		System.out.println("7단계 " + new_id);
-
-		String answer = new_id;
+	public static int solution(int[][] board, int[] moves) {
+		int answer = 0;
 		return answer;
 	}
 
-	public String solution2(String new_id) {
-        String answer = "";
-        String temp = new_id.toLowerCase();
-
-        temp = temp.replaceAll("[^-_.a-z0-9]","");
-        System.out.println(temp);
-        temp = temp.replaceAll("[.]{2,}",".");
-
-        temp = temp.replaceAll("^[.]|[.]$","");
-        System.out.println(temp.length());
-        
-		if(temp.equals(""))
-            temp+="a";
-        if(temp.length() >=16){
-            temp = temp.substring(0,15);
-            temp=temp.replaceAll("^[.]|[.]$","");
-        }
-        if(temp.length()<=2)
-            while(temp.length()<3)
-                temp+=temp.charAt(temp.length()-1);
-
-        answer=temp;
-        return answer;
-    }
 }
